5fa563a2810467a237d09e5e2bc1bc05201426ff
Chris Lane <pankokrumbs@gmail.com>
2020-02-07 14:05:43 -0700

    Background

---
+++ src/Components/resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Resume = () => {
+  return (
+    <main>
+      <aside>
+        <h1>Chris Lane</h1>
+        <p>Software Developer</p>
+      </aside>
+      <article></article>
+    </main>
+  )
+}
